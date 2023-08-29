9810b3505afeea8ee7b86be118f8a73d31b9b83c
Mussesan <guprmusse@gmail.com>
2023-08-29 20:58:41 -0300

    update: novo layout de projetos

--- src/components/Projects2.tsx
+++ src/components/Projects2.tsx
@@ -1,28 +1,28 @@
 export function Projects2(){
     const projectsItens = [
         {
-            title: 'TechHub-Blog',
+            title: 'Github API',
             imgUrl:
                 'https://raw.githubusercontent.com/ShaifArfan/techHub-blog/main/banner.png',
-            tech: ['React JS', 'Gatsby JS', 'Sanity.io'],
+            tech: ['React JS', 'CSS Modules', 'API'],
             workUrl: 'https://github.com/ShaifArfan/techHub-blog',
         },
         {
-            title: "Shaif'f Cuisine",
+            title: "some text",
             imgUrl:
                 'https://raw.githubusercontent.com/ShaifArfan/shaif-s-cuisine/main/readmeImg/banner.png',
             tech: ['HTML', 'CSS', 'Netlify'],
             workUrl: 'https://github.com/ShaifArfan/shaif-s-cuisine',
         },
         {
-            title: "Ayan's Portfolio Website",
+            title: "some text",
             imgUrl:
                 'https://raw.githubusercontent.com/ShaifArfan/AYANs-portfolio/main/ReadMeImages/ReadMeBanner.png',
             tech: ['React JS', 'CSS'],
             workUrl: 'https://github.com/ShaifArfan/AYANs-portfolio',
         },
         {
-            title: 'Artistic',
+            title: 'some text',
             imgUrl:
                 'https://raw.githubusercontent.com/ShaifArfan/artistic/main/readmeImg/banner.png',
             tech: ['React JS', 'CSS'],
@@ -31,7 +31,28 @@ export function Projects2(){
     ]
 
     return(<>
+        {projectsItens.map((item)=>{
+            return (<section className="flex flex-row bg-blue-600">
 
+                <div className=" gap-2">
+                    <div>
+                        <img 
+                            className="w-28 h-28"
+                            src={item.imgUrl} 
+                            alt="" 
+                        />
+                    </div>
+
+                    <div>
+                        <span>{item.title}</span>
+                    </div>
+
+                    <div>
+                        <span>{item.tech}</span>
+                    </div>
+                </div>
+            </section>)
+        })}
     
     </>)
 }
